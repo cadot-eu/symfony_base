278e9651c46b73d8899533d2558f149942cefe82
michael <michael@cadot.eu>
2025-03-28 08:50:08 +0100

    add rep en cours

--- refresh.sh
+++ refresh.sh
@@ -1,5 +1,5 @@
 while true; do
-    docker exec -it base bin/console asset-map:compile
+    docker exec -it $(basename "$(pwd)") bin/console asset-map:compile
     inotifywait -e modify,create,delete -r assets
     sleep 1
 done
