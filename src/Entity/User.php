74686a9478baf64e8c39c469afaded44899487c5
michael <michael@cadot.eu>
2025-03-28 20:08:45 +0100

    clean

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -3,8 +3,6 @@
 namespace App\Entity;
 
 use App\Repository\UserRepository;
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
@@ -44,16 +42,6 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column(type: Types::TEXT, nullable: true)]
     private ?string $note = null;
 
-    /**
-     * @var Collection<int, Test>
-     */
-    #[ORM\OneToMany(targetEntity: Test::class, mappedBy: 'usager')]
-    private Collection $tests;
-
-    public function __construct()
-    {
-        $this->tests = new ArrayCollection();
-    }
 
     public function getId(): ?int
     {
@@ -167,36 +155,6 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function setNote(?string $note): static
     {
         $this->note = $note;
-
-        return $this;
-    }
-
-    /**
-     * @return Collection<int, Test>
-     */
-    public function getTests(): Collection
-    {
-        return $this->tests;
-    }
-
-    public function addTest(Test $test): static
-    {
-        if (!$this->tests->contains($test)) {
-            $this->tests->add($test);
-            $test->setUsager($this);
-        }
-
-        return $this;
-    }
-
-    public function removeTest(Test $test): static
-    {
-        if ($this->tests->removeElement($test)) {
-            // set the owning side to null (unless already changed)
-            if ($test->getUsager() === $this) {
-                $test->setUsager(null);
-            }
-        }
 
         return $this;
     }
