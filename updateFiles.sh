e63bbb58c2563fb0c30ba34d11598ebe3b47e8d4
michael <michael@cadot.eu>
2025-04-05 17:25:17 +0200

    maj

--- updateFiles.sh
+++ updateFiles.sh
@@ -2,7 +2,7 @@
 
 # Récupère le nom du répertoire courant
 PROJECT_NAME=$(basename "$PWD")
-
+NUMPORT=$(shuf -i 8000-8999 -n 1)
 # Fichiers à modifier
 FILES=("Caddyfile" "compose.yaml" "compose.override.yaml")
 
@@ -10,10 +10,12 @@ FILES=("Caddyfile" "compose.yaml" "compose.override.yaml")
 for FILE in "${FILES[@]}"; do
     if [[ -f "$FILE" ]]; then
         echo "Mise à jour de $FILE..."
-        sed -i "s/\bbase\b/$PROJECT_NAME/g" "$FILE"
+        sed -i "s/DIRECTORY/$PROJECT_NAME/g" "$FILE"
+        sed -i "s/NUMPORT/$NUMPORT/g" "$FILE"
+        
     else
         echo "⚠️ Le fichier $FILE n'existe pas, il sera ignoré."
     fi
 done
 
-echo "✅ Remplacement terminé : 'base' → '$PROJECT_NAME' dans ${FILES[*]}"
+echo "✅ Remplacement terminé : 'DIRECTORY' → '$PROJECT_NAME' dans ${FILES[*]}"
